638003d75024779c284e9c3a6a504b3ef72468c1
yonasmekonen <bayamak7@gmail.com>
2026-08-08 17:09:39 +0300

    just check our username

---
+++ donut.js
@@ -0,0 +1,13 @@
+const myArray = ["A", "B", "C", "D"];
+
+function myAwesomeFunction(element, index, array) {
+  console.log("Element: ", element);
+  console.log("Index: ", index);
+  console.log("Array: ", array);
+}
+
+myArray.forEach(myAwesomeFunction);
+
+
+
+
